702e9bc46af8be85da4bc54f78e57f89a15c0131
a-chavez474 <abelinkpact@gmail.com>
2023-04-30 00:09:12 -0600

    updates

--- server.js
+++ server.js
@@ -10,7 +10,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use('/', require('./routes'));
 
